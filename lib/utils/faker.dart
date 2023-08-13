d095d1047b3633a472c81b9b5c8c23e7d956ef67
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-08-14 01:50:27 +0200

    Added faker

---
+++ lib/utils/faker.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+
+class AppFaker {
+  final Faker faker = Faker();
+
+  String generatePlaceholderImage() {
+    final placeholderImage = faker.image.image(
+      keywords: ['Dungeons and Dragons'],
+      random: true,
+    );
+    return placeholderImage;
+  }
+}
