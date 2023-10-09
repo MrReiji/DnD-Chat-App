4d5942331b8ae3b9c2105a76549465cee640e9b1
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-10-09 23:20:56 +0200

    resizeToAvoidBottomInset : true - in SignUpScreen

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,11 +1,11 @@
-import 'package:dnd_chat_app/blocs/form_bloc/login_form/login_form_bloc.dart';
+import 'package:dnd_chat_app/blocs/form_bloc/login_form_bloc.dart';
 import 'package:dnd_chat_app/utils/approuter_paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../blocs/cubits/image_picker/image_picker_cubit.dart';
-import '../blocs/form_bloc/signUp_form_bloc/signUp_form_bloc.dart';
+import '../blocs/form_bloc/signUp_form_bloc.dart';
 import '../models/image_picker.dart';
 import '../widgets/loading_dialog.dart';
 
@@ -23,7 +23,7 @@ class SignUpScreen extends StatelessWidget {
 
           return Scaffold(
             appBar: AppBar(),
-            resizeToAvoidBottomInset: false,
+            resizeToAvoidBottomInset: true,
             body: Center(
               child: FormBlocListener<SignUpFormBloc, String, String>(
                 onSubmitting: (context, state) {
