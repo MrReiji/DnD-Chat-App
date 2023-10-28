a4cb4d3331709cb29691c0dd64f8e025496675a2
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-10-29 00:46:29 +0200

    Added Firestore steam to update campaigns list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dnd_chat_app/blocs/form_bloc/add_campaign_form_bloc_bloc.dart';
+import 'package:dnd_chat_app/models/campaign.dart';
+import 'package:dnd_chat_app/utils/download_image_from_URL.dart';
+import 'package:dnd_chat_app/widgets/loading_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +12,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:go_router/go_router.dart';
 import '../blocs/campaign/campaign_bloc.dart';
 import '../utils/approuter_paths.dart';
+
+import '../models/icons/dnd_icons.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -34,9 +43,20 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: BlocBuilder<CampaignBloc, CampaignState>(
-        builder: (context, state) {
-          if (state is CampaignInitial) {
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('campaigns')
+            .where('creatorID',
+                isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (ctx, chatSnapshots) {
+          if (chatSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
             return Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -54,54 +74,77 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             );
-          } else if (state is CampaignLoaded) {
-            return ListView.builder(
-                itemCount: state.campaigns.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    onTap: () {
-                      context.push(AppRouterPaths.chat);
-                    },
-                    leading: SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: Image.file(state.campaigns[index].image),
-                    ),
-                    title: Text(state.campaigns[index].title),
-                    subtitle: Text(state.campaigns[index].description),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: IconButton(
-                              icon: Icon(Icons.edit),
-                              iconSize: 25,
-                              onPressed: () {
-                                context.push(AppRouterPaths.add_campaign,
-                                    extra: state.campaigns[index]);
-                              },
-                            ),
-                          ),
-                          Expanded(
-                            child: IconButton(
-                              icon: Icon(Icons.cancel),
-                              iconSize: 25,
-                              onPressed: () {
-                                context.read<CampaignBloc>().add(DeleteCampaign(
-                                    campaign: state.campaigns[index]));
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  );
-                });
-          } else {
-            return Text("Doesn't work");
           }
+
+          if (chatSnapshots.hasError) {
+            return Center(
+              child: Text(
+                'Something went wrong...',
+                style: GoogleFonts.preahvihear(fontSize: 20),
+              ),
+            );
+          }
+
+          final loadedCampaigns = chatSnapshots.data!.docs;
+
+          return ListView.builder(
+            itemCount: loadedCampaigns.length,
+            itemBuilder: (BuildContext context, int index) {
+              final campaign = loadedCampaigns[index].data();
+              final String id = loadedCampaigns[index].id;
+              final String title = campaign['title'];
+              final String description = campaign['description'];
+              final String image_url = campaign['image_url'];
+              final String creatorID = campaign['creatorID'];
+              return ListTile(
+                onTap: () {
+                  context.push(AppRouterPaths.chat);
+                },
+                leading: SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.network(image_url),
+                ),
+                title: Text(title),
+                subtitle: Text(description),
+                trailing: SizedBox(
+                  width: 100,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: IconButton(
+                          icon: Icon(Icons.edit),
+                          iconSize: 25,
+                          onPressed: () async {
+                            LoadingDialog();
+                            final imageFile =
+                                await downloadImage(Uri.parse(image_url), id);
+                            context.push(AppRouterPaths.add_campaign,
+                                extra: Campaign(
+                                    id: id,
+                                    title: title,
+                                    description: description,
+                                    image: imageFile,
+                                    creatorID: creatorID));
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          icon: Icon(Icons.cancel),
+                          iconSize: 25,
+                          onPressed: () {
+                            deleteCampaignAndFile(id);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10),
+              );
+            },
+          );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -112,15 +155,15 @@ class HomeScreen extends StatelessWidget {
         type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: "Chats",
+            icon: Icon(DnDIcons.burning_book),
+            label: "Campaigns",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.group_work),
-            label: "Channels",
+            icon: Icon(DnDIcons.perspective_dice_six),
+            label: "Table",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
+            icon: Icon(DnDIcons.knight_helmet),
             label: "Profile",
           ),
         ],
