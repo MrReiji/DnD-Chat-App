a81c95f3322cdc3de42a9263a938963ec1c22820
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-10-29 00:39:39 +0200

    Added DnDIcons from FlutterIcon

---
+++ lib/models/icons/dnd_icons.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+
+class DnDIcons {
+  DnDIcons._();
+
+  static const _kFontFam = 'DnDIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData bat_sword =
+      IconData(0xe91b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData broadsword =
+      IconData(0xe946, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData burning_book =
+      IconData(0xe94e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData croc_sword =
+      IconData(0xe977, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData crossed_swords =
+      IconData(0xe97d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dragon =
+      IconData(0xe9a2, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dripping_sword =
+      IconData(0xe9a7, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData fireball_sword =
+      IconData(0xe9c4, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData helmet =
+      IconData(0xe9fe, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData knight_helmet =
+      IconData(0xea1a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData perspective_dice_six =
+      IconData(0xea5e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData two_dragons =
+      IconData(0xead9, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
