b7ebd932ef6c07f3a84d8a7ae5ed6a7bc54ec017
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-11-05 17:19:31 +0100

    Fixed problem with App_AppBar

--- lib/widgets/app_AppBar.dart
+++ lib/widgets/app_AppBar.dart
@@ -4,17 +4,14 @@ import 'package:google_fonts/google_fonts.dart';
 class App_AppBar extends AppBar {
   final List<Widget>? actions;
 
-  App_AppBar({super.key, this.actions});
-
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Center(
-        child: Text(
-          "DnD Chat App",
-          style: GoogleFonts.outfit(fontSize: 30),
-        ),
-      ),
-      actions: actions,
-    );
-  }
+  App_AppBar({Key? key, this.actions})
+      : super(
+            key: key,
+            title: Center(
+              child: Text(
+                "DnD Chat App",
+                style: GoogleFonts.outfit(fontSize: 30),
+              ),
+            ),
+            actions: actions);
 }
