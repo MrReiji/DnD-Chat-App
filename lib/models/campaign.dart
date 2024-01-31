61b1223e2211ef606abe20fb8034a0e1d6d0d68a
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2024-02-01 00:55:20 +0100

    Campaign documentation

--- lib/models/campaign.dart
+++ lib/models/campaign.dart
@@ -2,6 +2,17 @@ import 'dart:io';
 
 import 'package:equatable/equatable.dart';
 
+/// Represents a campaign model.
+///
+/// A campaign is a collection of information including its unique [id],
+/// [title], [description], [image], [creatorID], and [participants].
+///
+/// - The [id] is a unique identifier for the campaign.
+/// - The [title] is the title of the campaign.
+/// - The [description] is a brief description of the campaign.
+/// - The [image] is the image associated with the campaign.
+/// - The [creatorID] is the ID of the user who created the campaign.
+/// - The [participants] is a list of IDs of the users participating in the campaign.
 class Campaign extends Equatable {
   final String id;
   final String title;
